d48ee1bc9fdbe2e97e40b6fe3a40339f6f69e77c
631916165 <64794333+631916165@users.noreply.github.com>
2023-08-17 17:32:25 +0800

    Add files via upload

---
+++ springbootHello/src/main/java/com/example/SpringBoot01HelloQuickApplication.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot01HelloQuickApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot01HelloQuickApplication.class, args);
+    }
+}
