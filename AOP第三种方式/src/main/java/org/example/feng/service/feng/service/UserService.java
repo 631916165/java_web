7f47406539f14f3743b0a66a7b6aa9fbe80546f6
631916165 <64794333+631916165@users.noreply.github.com>
2023-08-21 15:10:05 +0800

    Delete AOP第三种方式 directory

--- AOP第三种方式/src/main/java/org/example/feng/service/feng/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package org.example.feng.service.feng.service;
-
-public interface UserService {
-    public void add();
-    public void delete();
-    public void update();
-    public void select();
-}
