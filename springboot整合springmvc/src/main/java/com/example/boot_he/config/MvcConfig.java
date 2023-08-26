fd85b1399f6642d3eb0cae40d9e93f25cce06dd5
631916165 <64794333+631916165@users.noreply.github.com>
2023-08-26 10:58:56 +0800

    Add files via upload

---
+++ springboot整合springmvc/src/main/java/com/example/boot_he/config/MvcConfig.java
@@ -0,0 +1,20 @@
+package com.example.boot_he.config;
+
+
+import com.example.boot_he.interceptor.MyInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private MyInterceptor myInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(myInterceptor).addPathPatterns("/**");
+    }
+}
---
+++ springboot整合springmvc/src/main/java/com/example/boot_he/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.example.boot_he.controller;
+
+
+import com.example.boot_he.pojo.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/getuser")
+    @ResponseBody
+    public User getuser() {
+        User user = new User();
+        user.setUsername("杨过");
+        user.setPassword("123456");
+        user.setAge(18);
+        user.setSex("男");
+        return user;
+    }
+}
